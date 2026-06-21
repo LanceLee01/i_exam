2d1a45717ce215b99d72e8205d706136a24c974d
Like <like@hermes.local>
2026-06-21 18:45:16 +0800

    chore: bump version to 3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.examhelper.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "3.0.0"
+        versionCode = 6
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
